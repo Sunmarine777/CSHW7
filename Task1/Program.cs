d9528a9695f97082e7df189a9245dd60066ea6e6
Sunmarine <Sunmarine777@yandex.ru>
2023-12-10 21:45:32 +0300

    Task1 from M to N worked out

---
+++ Task1/Program.cs
@@ -0,0 +1,39 @@
+﻿//Задача 1: Задайте значения M и N. Напишите программу, которая выведет все
+//натуральные числа в промежутке от M до N. Использовать рекурсию, 
+//не использовать циклы.
+
+
+using System.ComponentModel;
+using System.Net.Mail;
+
+void NaturalNumbersFromTo(int M, int N)
+{
+      if(M > N) 
+      return;
+      Console.Write(M + " ");
+      NaturalNumbersFromTo(M + 1, N);
+}
+
+int M = 5;
+int N = 10;
+NaturalNumbersFromTo(M,N);
+
+/*
+int ReadInt(string str)
+{
+      Console.Write(str);
+      return Convert.ToInt32(Console.ReadLine());
+}
+
+void NaturalNumbersFromTo(int M, int N)
+{
+      if(M > N) 
+      return;
+      Console.Write(M + " ");
+      NaturalNumbersFromTo(M + 1, N);
+}
+
+int M = ReadInt("Enter number M: ");
+int N = ReadInt("Enter number N: ");
+NaturalNumbersFromTo(M,N);
+*/
