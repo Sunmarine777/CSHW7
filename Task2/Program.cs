f52351e61aaf60fd4cb2c07ca8206d444a6c8e4e
Sunmarine <Sunmarine777@yandex.ru>
2023-12-10 23:46:00 +0300

    With some error

---
+++ Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+Даны два неотрицательных числа m и n.
+m = 3, n = 2 -> A(m,n) = 29*/
+
+using System;
+
+
+static int AkkermanFunc(int m, int n)
+{
+      if (m == 0) return n + 1;
+
+      if (m > 0 && n = 0) return AkkermanFunc(m - 1, 1);
+
+      if (m > 0 && n > 0) return AkkermanFunc(m - 1, AkkermanFunc(m, n - 1));
+
+      return 0;
+
+      Console.WriteLine(AkkermanFunc(m,n));
+}
+
+int m = 3, n = 2;
+
+AkkermanFunc(m,n);
