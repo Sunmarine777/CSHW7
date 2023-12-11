c33e49762d6d94adac8fd4769f1c14ad210e8f61
Sunmarine <Sunmarine777@yandex.ru>
2023-12-11 20:28:41 +0300

    Task2 Ackermann function done

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -4,20 +4,19 @@ m = 3, n = 2 -> A(m,n) = 29*/
 
 using System;
 
+int m = 3, n = 2;
 
-static int AkkermanFunc(int m, int n)
+static int AckermannFunc(int m, int n)
 {
       if (m == 0) return n + 1;
 
-      if (m > 0 && n = 0) return AkkermanFunc(m - 1, 1);
+      if (m > 0 && n == 0) return AckermannFunc(m - 1, 1);
 
-      if (m > 0 && n > 0) return AkkermanFunc(m - 1, AkkermanFunc(m, n - 1));
+      if (m > 0 && n > 0) return AckermannFunc(m - 1, AckermannFunc(m, n - 1));
 
-      return 0;
-
-      Console.WriteLine(AkkermanFunc(m,n));
+      return 0;    
 }
 
-int m = 3, n = 2;
+Console.WriteLine(AckermannFunc(m,n));
 
-AkkermanFunc(m,n);
+AckermannFunc(m,n);
